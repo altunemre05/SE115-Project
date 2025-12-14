ae5cf4878bc26c0195f4f589c4e5198597c8ccf8
altunemre05 <altnemre05@gmail.com>
2025-12-14 19:37:00 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,58 @@ public class Main {
     static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
     static String[] months = {"January","February","March","April","May","June",
                               "July","August","September","October","November","December"};
+    static int[][][] profits=new int[12][28][5];
     
 
     // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
     public static void loadData() {
+        for(int m=0;m<months.length;m++) {
+            try {
+                FileReader fr = new FileReader("Data_Files/" + months[m] + ".txt");
+                BufferedReader br = new BufferedReader(fr);
+                String line;
+                line=br.readLine();
+                while((line=br.readLine())!=null){
+                    String[] parts=line.split(",");
+
+                    int day = Integer.parseInt(parts[0])-1;
+                    String commodity = parts[1];
+                    int profit = Integer.parseInt(parts[2]);
+                    int cIndex = -1;
+
+                    for (int i = 0; i < commodities.length; i++) {
+                        if (commodities[i].equals(commodity)) {
+                            cIndex = i;
+                            break;
+                        }
+                    }
+                    if (cIndex != -1 && day >= 0 && day < 28) {
+                        profits[m][day][cIndex] = profit;
+                    }
+                }
+                br.close();
+            }
+            catch (Exception e) {
+             System.out.println("Error.");
+
+            }
+        }
     }
+    public static void infoS() {
+        for (int i = 0; i < 12; i++) {
+
+            for(int j=0;j<28;j++){
+
+                for(int l=0;l<5;l++){
+                    System.out.print(profits[i][j][l]);
+                    System.out.println();
+                }
+
+            }
+
+        }
+    }
+
 
     // ======== 10 REQUIRED METHODS (Students fill these) ========
 
@@ -60,5 +107,6 @@ public class Main {
     public static void main(String[] args) {
         loadData();
         System.out.println("Data loaded – ready for queries");
+        infoS();
     }
 }
